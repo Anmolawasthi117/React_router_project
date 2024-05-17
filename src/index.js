c7c875d009ddf7bb3a9f2fc7757b30cd58bf544e
Anmol <anmolawasthi117@gmail.com>
2024-05-17 16:18:29 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import reportWebVitals from './reportWebVitals';
+import { Route, RouterProvider, createRoutesFromElements } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact us/Contact';
+import User from './components/User/User';
+import Github from './components/Github/Github';
+// const router = createBrowserRouter([
+//   {
+//     path:'/',
+//     element:<Layout/>,
+//     children:[
+//       {
+//         path:"",
+//         element:<Home/>
+//       },
+//       {
+//         path:"about",
+//         element:<About/>
+//       },
+//       {
+//         path:"contact",
+//         element:<Contact/>
+//       }
+//     ]
+//   }
+// ])
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+    <Route path='' element={<Home/>}/>
+    <Route path='about' element={<About/>}/>
+    <Route path='contact' element={<Contact/>}/>
+    <Route path='user/:userid' element={<User/>}/>
+    <Route path='Github' element={<Github/>}/>
+
+    </Route>
+  )
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <RouterProvider router={router} />
   </React.StrictMode>
 );
 
